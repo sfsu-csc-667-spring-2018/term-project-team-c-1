13aa1945aa85ed7ca98452220fb9b76ac5e2c12e
Tyranicangel <prad.ads1990@gmail.com>
2018-03-22 12:01:59 -0700

    UI for Home

--- front/src/app/game/game.module.ts
+++ front/src/app/game/game.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { LobbyComponent } from './lobby/lobby.component';
 import { TableComponent } from './table/table.component';
 import { GameComponent } from './game/game.component';
 
+import { GameRouting } from './game.routing';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    GameRouting,
+    FormsModule
   ],
   declarations: [LobbyComponent, TableComponent, GameComponent]
 })
